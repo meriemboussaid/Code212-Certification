1980a332d94a00074915212981dcf97e2475e36d
meriemboussaid <meriemboussaid451@gmail.com>
2026-05-05 02:58:34 +0100

    Update 2025_04_20_000001_create_certifications_table.php

--- backend/database/migrations/2025_04_20_000001_create_certifications_table.php
+++ backend/database/migrations/2025_04_20_000001_create_certifications_table.php
@@ -1,22 +1,21 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     public function up(): void
     {
         Schema::create('certifications', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('titre');
             $table->text('description');
-            $table->string('duration');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->string('photo')->nullable();
             $table->timestamps();
         });
     }
-
     public function down(): void
     {
         Schema::dropIfExists('certifications');
