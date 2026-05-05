767935317e944c30990a92130d3232d750ea2bc0
meriemboussaid <meriemboussaid451@gmail.com>
2026-05-05 16:11:54 +0100

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EnrollmentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 🚀 LE COUP DE GRÂCE : On force la route ici !
+Route::get('api/admin/enrollments', [EnrollmentController::class, 'index'])->middleware('auth:sanctum');
